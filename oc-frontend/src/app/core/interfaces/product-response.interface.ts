fdf08902425b3b99ec6d9e8fd9d3a372ca6cde95
erick <erickva2512@gmail.com>
2026-04-22 16:03:00 -0500

    feat: Add ProductResponse interface and extend ProviderSelection with providerRuc

---
+++ oc-frontend/src/app/core/interfaces/product-response.interface.ts
@@ -0,0 +1,9 @@
+export interface ProductResponse {
+  id: number;
+  nombre: string;
+  precio: number | string;
+  proveedorId: number;
+  proveedorRuc: string;
+  servicioId: number;
+  servicioNombre: string;
+}
--- oc-frontend/src/app/core/interfaces/provider-option.interface.ts
+++ oc-frontend/src/app/core/interfaces/provider-option.interface.ts
@@ -12,4 +12,5 @@ export interface ProviderSelection {
     serviceName: string;
     providerId: number;
     providerName: string;
+    providerRuc: string;
 }

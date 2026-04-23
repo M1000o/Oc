042d08bc5b294907748f45dacb13cc25997e8e5f
erick <erickva2512@gmail.com>
2026-04-23 17:54:18 -0500

    feat: Add productos-registration component with HTML, CSS, and TypeScript files

---
+++ oc-frontend/src/app/features/productos-registration/productos-registration.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-productos-registration',
+  imports: [],
+  templateUrl: './productos-registration.html',
+  styleUrl: './productos-registration.css',
+})
+export class ProductosRegistration {
+
+}

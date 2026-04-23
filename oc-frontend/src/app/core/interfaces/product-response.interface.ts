f54987f6857ada77f4e53334c3d6715a927b7cdb
erick <erickva2512@gmail.com>
2026-04-23 17:52:44 -0500

    feat: Add missing fields codigo_producto and und_medida to ProductResponse interface

--- oc-frontend/src/app/core/interfaces/product-response.interface.ts
+++ oc-frontend/src/app/core/interfaces/product-response.interface.ts
@@ -1,8 +1,10 @@
 export interface ProductResponse {
   id: number;
+  codigo_producto: string;
   nombre: string;
   precio: number | string;
   proveedorId: number;
+  und_medida: string;
   proveedorRuc: string;
   servicioId: number;
   servicioNombre: string;

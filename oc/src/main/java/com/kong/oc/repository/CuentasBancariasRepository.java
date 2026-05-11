7463a0b1cf59169764192ead5effd0c4cc1e3e6e
erick <erickva2512@gmail.com>
2026-05-11 14:59:29 -0500

    feat(cuentas-bancarias): add methods to check existence by numeroCuenta and cci

--- oc/src/main/java/com/kong/oc/repository/CuentasBancariasRepository.java
+++ oc/src/main/java/com/kong/oc/repository/CuentasBancariasRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CuentasBancariasRepository extends JpaRepository<CuentasBancarias, Long> {
+
+    boolean existsByNumeroCuenta(String numeroCuenta);
+    boolean existsByCci(String cci);
 }

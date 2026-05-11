e4b841638bd74cf76546dd04abc4be1624d366e4
erick <erickva2512@gmail.com>
2026-05-11 15:03:44 -0500

    feat(configuration): add configuration page with styles and layout

---
+++ oc-frontend/src/app/features/configuration/configuration.page.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-configuration-page',
+  imports: [CommonModule],
+  templateUrl: './configuration.page.html',
+  styleUrl: './configuration.page.css'
+})
+export class ConfigurationPage {}

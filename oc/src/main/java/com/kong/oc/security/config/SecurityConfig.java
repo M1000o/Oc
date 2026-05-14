3d0b4f6bcfcef5b82fccf986f5485168f38711aa
Erick <erickva2512@gmail.com>
2026-05-14 00:43:28 -0500

    feat(security): Update allowed methods in CORS configuration to include PATCH

--- oc/src/main/java/com/kong/oc/security/config/SecurityConfig.java
+++ oc/src/main/java/com/kong/oc/security/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(request -> {
                     var config = new CorsConfiguration();
                     config.setAllowedOriginPatterns(List.of("http://localhost:4200"));
-                    config.setAllowedMethods(List.of("GET","POST", "PUT", "DELETE", "OPTIONS"));
+                    config.setAllowedMethods(List.of("GET","POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
                     config.setAllowedHeaders(List.of("*"));
                     config.setExposedHeaders(List.of(
                             "Authorization",
